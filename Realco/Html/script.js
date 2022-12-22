092fda7f98e55ff05aa672f7bbc59a90eeb464e1
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2022-12-22 23:30:53 +0600

    Add files via upload

---
+++ Realco/Html/script.js
@@ -0,0 +1,9 @@
+'use strict'
+const menuIcon = document.querySelector(".menu__icon");
+if (menuIcon) {
+   const headMenu = document.querySelector(".head__main ");
+   menuIcon.addEventListener("click", function () {
+      menuIcon.classList.toggle("_active");
+      headMenu.classList.toggle("_actived");
+   });
+}
