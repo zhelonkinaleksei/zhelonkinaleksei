268e4200b78f4a72d5ebaea4051d5f4b06a92824
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2022-12-22 20:34:04 +0300

    Delete Realco directory

--- Realco/Html/script.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const menuIcon = document.querySelector(".menu__icon");
-if (menuIcon) {
-   const headMenu = document.querySelector(".head__main ");
-   menuIcon.addEventListener("click", function () {
-      menuIcon.classList.toggle("_active");
-      headMenu.classList.toggle("_actived");
-   });
-}
